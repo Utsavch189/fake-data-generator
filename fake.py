833b4e92800009cb74d4a9a783818406074a0a73
Utsav Chatterjee <88131195+Utsavch189@users.noreply.github.com>
2021-12-08 21:50:22 +0530

    Update fake.py

--- fake.py
+++ fake.py
@@ -5,44 +5,20 @@ import random
 from datetime import datetime
 import csv
 
-#def fake():
-    
-    #f=Faker()
-    
-    #obj=set()
- 
-    #out=[{
-         #nu.random.randint(2010,2021),
-         #33,
-         #nu.random.random_integers(1,2),
-         #nu.random.random_integers(2,5)
-    
-    #}]
-    #for i in range(1,101):
-        #obj.add(out)
-        #return obj
-   
-    
-    
+def value():
+    l=values=[nu.random.randint(2010,2021),3,nu.random.random_integers(1,2),nu.random.random_integers(2,5)]
+    return l
 
+def write_file(file_name,data):
+    file=open(file_name,'a',newline='')
+    writer=csv.writer(file)
+    writer.writerow(data)
 
-with open('dat.csv', 'w', encoding='UTF8',newline='') as f:
-    # create the csv writer
-    writer = csv.writer(f)
-    keys=['year','state','Suspected_object','People_Involved']   
-    # write a row to the csv file
-    #writer.writerow(fake())
-    count=0
-    for data in range(1,100):
-        values=[nu.random.randint(2010,2021),2,nu.random.random_integers(1,2),nu.random.random_integers(2,5)]
-        
-        if count == 0:
-            header = keys
-            writer.writerow(header)
-            count += 1
-            
-        
-        writer.writerow(values)
+keys=['year','state','Suspected_object','People_Involved']
+
+#write_file('test.csv',keys)
+for i in range(0,100):
+    write_file('test.csv',value())
  
       
         
